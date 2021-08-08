c63f0714fad8d3ff05779182e803c037669e8cd3
Obaida Alnakdali <O.Alnakdali@gmail.com>
2021-08-08 15:26:19 +0300

    obaida first change

--- Backend/controllers/gifts.js
+++ Backend/controllers/gifts.js
@@ -1,20 +1,32 @@
 const User = require("../models/user");
+const Gifts = require("../models/gifts");
 
 exports.postAddGifts = (req, res, next) => {
+  console.log(req.body);
   const title = req.body.title;
   const imgUrl = req.body.imgUrl;
   const description = req.body.description;
-  User.findOne({ where: { id: req.userId } })
+  const location = req.body.location;
+
+    User.findOne({ where: { id: req.userId } })
     .then((user) => {
-      console.log("hey");
-      user.createGifts({ title, imgUrl, description });
+        console.log("hey");
+        user.createGifts();({ title, imgUrl, description, loacation });
     })
-    .then((results) => res.send("Hobby is created succesfully"))
+    .then((results) => res.send("Gift is created succesfully"))
     .catch((err) => console.log(err));
 };
+
 exports.getGifts = (req, res, next) => {
   User.findOne({ where: { id: req.userId } })
-    .then((user) => user.getGifts())
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
+  .then((user) => user.getGifts())
+  .then((result) => res.send(result))
+  .catch((err) => console.log(err));
+};
+
+exports.deleteGift = async (req, res, next) => {
+  Gift.findOne({ where: { id: req.body.id } })
+  .then((gifts) => gifts.deleteGift(id))
+  .then((result) => res.send(result))
+  .catch((err) => console.log(err));
 };
