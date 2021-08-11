e323d5075e9cada4015489a42a75dd537cce400a
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-08-11 14:44:27 +0300

    fix the get-all-gift

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("giftify", "root", "", {
+module.exports = new Sequelize("new-giftify", "root", "CODing94spACE", {
   host: "localhost",
   dialect: "mysql",
 });
