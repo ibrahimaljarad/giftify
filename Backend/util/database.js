e7b83c3da5d5ce867eed554f43af35e04b28b563
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-08-11 14:46:02 +0300

    my sql config

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("new-giftify", "root", "CODing94spACE", {
+module.exports = new Sequelize("new-giftify", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
